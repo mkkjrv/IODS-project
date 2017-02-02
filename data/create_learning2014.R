ec49cd973cf84e6a3d298f59c00425070959ca6a
Mikko Järvi <mikko.jarvi@helsinki.fi>
2017-02-02 21:45:17 +0200

    data file
    reading some data from internet

---
+++ data/create_learning2014.R
@@ -0,0 +1,14 @@
+#Mikko Järvi 30.1.2017. This is a R script file for week 2 exercise.
+
+#reading the learning data from internet
+learning2014 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep = "\t", header = TRUE )
+
+#exploring dimension of the data dim() = rows, columns 
+dim(learning2014)
+
+#structure of the data
+str(learning2014)
+
+getwd()
+
+
