22fae85e89944181b4d29f0c270bbd1699d13e82
Mikko Järvi <mikko.jarvi@helsinki.fi>
2017-02-02 22:50:58 +0200

    data wrangling 3

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -3,12 +3,33 @@
 #reading the learning data from internet
 learning2014 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep = "\t", header = TRUE )
 
-#exploring dimension of the data dim() = rows, columns 
+#exploring dimension of the data with dim(), output = rows, columns 
 dim(learning2014)
 
 #structure of the data
+#data includes 183 observations and 60 variables, most of variables are integers
 str(learning2014)
 
-getwd()
+#questions related to deep, surface and strategic learning according to the datacamp exercise
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
 
+#select the columns related to the component in hand and creating columns by averaging  
+deep_columns <- select(learning2014, one_of(deep_questions))
+learning2014$deep <- rowMeans(deep_columns)
 
+surface_columns <- select(learning2014, one_of(surface_questions))
+learning2014$surf <- rowMeans(surface_columns)
+
+startegic_columns <- select(learning2014, one_of(strategic_questions))
+learning2014$stra <- rowMeans(startegic_columns)
+
+#choose relevant columns to keep
+keep_col <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+
+#create a new dataset from 'keep_col'
+learning2014 <- select(learning2014, one_of(keep_col))
+
+#check the new dataset learning2014
+str(learning2014)
