4ae44b427495d215bd4fb35e0eef93d1be0da298
Mikko Järvi <mikko.jarvi@helsinki.fi>
2017-02-10 22:03:06 +0200

    joining the files

--- data/create_alc.R
+++ data/create_alc.R
@@ -1,1 +1,24 @@
 #Mikko Järvi 10.2.2017. This is a script file for the week 3 exercise.
+
+#tools
+library(dplyr)
+
+#reading the data from csv files
+math <- read.csv("~/mikko/IODS-project/data/student-mat.csv", sep = ";", header = TRUE)
+por <- read.csv("~/mikko/IODS-project/data/student-por.csv", sep = ";", header = TRUE)
+
+#exploring the data of both files
+dim(math)
+dim(por)
+
+structure(math)
+structure(por)
+
+#joining the two files with common columns as identifiers
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+#joining the two datasets by the selected identifiers
+math_por <- inner_join(math, por, by = join_by, suffix = c(".math", ".por"))
+
+#checking the result
+colnames(math_por)
